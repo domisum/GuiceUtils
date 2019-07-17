76a56e6edc0acb91e166f98a3b6e8d1d0035175b
domisum <dominik.weissenseel@gmail.com>
2019-07-17 19:48:22 +0200

    Initial commit

---
+++ src/main/java/de/domisum/guiceutils/GuiceConstructorBinder.java
@@ -0,0 +1,36 @@
+package de.domisum.guiceutils;
+
+import com.google.inject.Binder;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class GuiceConstructorBinder
+{
+
+	public static <I> void bindToOnlyOwn(Binder binder, Class<I> implementingClass)
+	{
+		bindToOnly(binder, implementingClass, implementingClass);
+	}
+
+	public static <A, I extends A> void bindToOnly(
+			Binder binder, Class<A> abstractClass, Class<I> implementingClass)
+	{
+		@SuppressWarnings("unchecked")
+		Constructor<I>[] constructorsArray = (Constructor<I>[]) implementingClass.getConstructors();
+		List<Constructor<I>> constructors = Arrays.asList(constructorsArray);
+
+		if(constructors.isEmpty())
+			throw new IllegalArgumentException("no constructor to bind to found in class "+implementingClass.getName());
+		if(constructors.size() > 1)
+			throw new IllegalArgumentException("too many constructors to bind to in class "+implementingClass.getName());
+
+		Constructor<I> constructor = constructors.get(0);
+		binder.bind(abstractClass).toConstructor(constructor);
+	}
+
+}
---
+++ src/main/java/de/domisum/guiceutils/GuiceMultibinder.java
@@ -0,0 +1,37 @@
+package de.domisum.guiceutils;
+
+import com.google.inject.Binder;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class GuiceMultibinder
+{
+
+	@SafeVarargs
+	public static <T> void multibind(Binder binder, Class<T> parent, Class<? extends T>... children)
+	{
+		multibind(binder, parent, Arrays.asList(children));
+	}
+
+	public static <T> void multibind(Binder binder, Class<T> parent, Collection<Class<? extends T>> children)
+	{
+		Multibinder<T> setBinder = Multibinder.newSetBinder(binder, parent);
+		for(Class<? extends T> aClass : children)
+			setBinder.addBinding().to(aClass);
+	}
+
+	@SafeVarargs
+	public static <T> void multibind(Binder binder, TypeLiteral<T> parent, Class<? extends T>... children)
+	{
+		Multibinder<T> setBinder = Multibinder.newSetBinder(binder, parent);
+		for(Class<? extends T> aClass : children)
+			setBinder.addBinding().to(aClass);
+	}
+
+}
