4fc18b2e0724bf108b7563953e6a08996b86d451
domisum <dominik.weissenseel@gmail.com>
2020-03-24 22:43:44 +0100

    Cleaned up

--- src/main/java/io/domisum/lib/guiceutils/GuiceConstructorBinder.java
+++ src/main/java/io/domisum/lib/guiceutils/GuiceConstructorBinder.java
@@ -1,36 +1,38 @@
 package io.domisum.lib.guiceutils;
 
 import com.google.inject.Binder;
+import io.domisum.lib.auxiliumlib.annotations.API;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
-import java.util.List;
 
+@API
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GuiceConstructorBinder
 {
-
+	
+	@API
 	public static <I> void bindToOnlyOwn(Binder binder, Class<I> implementingClass)
 	{
 		bindToOnly(binder, implementingClass, implementingClass);
 	}
-
-	public static <A, I extends A> void bindToOnly(
-			Binder binder, Class<A> abstractClass, Class<I> implementingClass)
+	
+	@API
+	public static <A, I extends A> void bindToOnly(Binder binder, Class<A> abstractClass, Class<I> implementingClass)
 	{
 		@SuppressWarnings("unchecked")
 		Constructor<I>[] constructorsArray = (Constructor<I>[]) implementingClass.getConstructors();
-		List<Constructor<I>> constructors = Arrays.asList(constructorsArray);
-
+		var constructors = Arrays.asList(constructorsArray);
+		
 		if(constructors.isEmpty())
 			throw new IllegalArgumentException("no constructor to bind to found in class "+implementingClass.getName());
 		if(constructors.size() > 1)
 			throw new IllegalArgumentException("too many constructors to bind to in class "+implementingClass.getName());
-
-		Constructor<I> constructor = constructors.get(0);
+		
+		var constructor = constructors.get(0);
 		binder.bind(abstractClass).toConstructor(constructor);
 	}
-
+	
 }
--- src/main/java/io/domisum/lib/guiceutils/GuiceMultibinder.java
+++ src/main/java/io/domisum/lib/guiceutils/GuiceMultibinder.java
@@ -3,59 +3,40 @@ package io.domisum.lib.guiceutils;
 import com.google.inject.Binder;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
-import io.domisum.lib.auxiliumlib.PHR;
-import io.domisum.lib.auxiliumlib.util.java.ClassUtil;
+import io.domisum.lib.auxiliumlib.annotations.API;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 
+@API
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GuiceMultibinder
 {
-
+	
+	@API
 	@SafeVarargs
 	public static <T> void multibind(Binder binder, Class<T> parent, Class<? extends T>... children)
 	{
 		multibind(binder, parent, Arrays.asList(children));
 	}
-
+	
+	@API
 	public static <T> void multibind(Binder binder, Class<T> parent, Collection<Class<? extends T>> children)
 	{
 		var setBinder = Multibinder.newSetBinder(binder, parent);
-		for(Class<? extends T> aClass : children)
-			setBinder.addBinding().to(aClass);
+		for(var child : children)
+			setBinder.addBinding().to(child);
 	}
-
+	
+	@API
 	@SafeVarargs
 	public static <T> void multibind(Binder binder, TypeLiteral<T> parent, Class<? extends T>... children)
 	{
 		var setBinder = Multibinder.newSetBinder(binder, parent);
-		for(Class<? extends T> aClass : children)
-			setBinder.addBinding().to(aClass);
+		for(var child : children)
+			setBinder.addBinding().to(child);
 	}
-
-	@SuppressWarnings("unchecked")
-	public static <T> void multibind(Binder binder, Class<T> parent, String childrenPackage)
-	{
-		var packageClasses = ClassUtil.getClasses(childrenPackage);
-
-		if(packageClasses.isEmpty())
-			throw new IllegalArgumentException("package is empty: "+childrenPackage);
-
-		var childrenClasses = new HashSet<Class<? extends T>>();
-		for(Class<?> clazz : packageClasses)
-			if(parent.isAssignableFrom(clazz))
-				childrenClasses.add((Class<? extends T>) clazz);
-			else
-				throw new IllegalArgumentException(PHR.r("childrenPackage contained class which is not a child of {}: {}",
-						parent.getName(),
-						clazz.getName()
-				));
-
-		multibind(binder, parent, childrenClasses);
-	}
-
+	
 }
